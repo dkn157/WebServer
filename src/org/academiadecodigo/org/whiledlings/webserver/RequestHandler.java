cc68987943711317ab15c439a73417a84cccff8b
Diogo Noronha <diogoknoronha@gmail.com>
2019-06-25 21:23:15 +0100

    minor changes

--- src/org/academiadecodigo/org/whiledlings/webserver/RequestHandler.java
+++ src/org/academiadecodigo/org/whiledlings/webserver/RequestHandler.java
@@ -25,17 +25,17 @@ public class RequestHandler implements Runnable {
         System.out.println(clientSocket.getRemoteSocketAddress());
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String str = in.readLine();
-        String[] s;
+        String[] strings;
         if (str == null || str.isEmpty()) {
             closeClientSocket();
         } else {
-            s = str.split(" ");
-            if (!s[0].equals("GET")) {
+            strings = str.split(" ");
+            if (!strings[0].equals("GET")) {
                 sendPage("/notallowed.html", Headers.NOTALLOWED);
                 return;
             }
-            if (s.length > 1) {
-                getRequested(s[1]);
+            if (strings.length > 1) {
+                getRequested(strings[1]);
             } else {
                 DataOutputStream write = new DataOutputStream(clientSocket.getOutputStream());
                 write.writeBytes(Headers.BADREQUEST.getHeaderContent());
@@ -64,30 +64,30 @@ public class RequestHandler implements Runnable {
     }
 
     private void sendPage(String str, Headers header) {
-        BufferedOutputStream out = null;
+        BufferedOutputStream bfout = null;
         str = ROOT + str;
         String s;
         s = header.getHeaderContent();
         s = s.replace("<file_byte_size>", new File(str).length() + "");
 
         try {
-            out = new BufferedOutputStream(clientSocket.getOutputStream());
-            out.write(s.getBytes());
+            bfout = new BufferedOutputStream(clientSocket.getOutputStream());
+            bfout.write(s.getBytes());
 
             BufferedReader bReader = new BufferedReader(new FileReader(str));
             String s1;
             while ((s1 = bReader.readLine()) != null) {
                 s1 += "\n";
-                out.write(s1.getBytes());
+                bfout.write(s1.getBytes());
             }
-            out.flush();
+            bfout.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (out != null) {
-                    out.close();
+                if (bfout != null) {
+                    bfout.close();
                 }
                 closeClientSocket();
             } catch (IOException e) {
@@ -97,29 +97,29 @@ public class RequestHandler implements Runnable {
     }
 
     private void sendImage(String str) {
-        BufferedOutputStream out = null;
+        BufferedOutputStream bfout = null;
         str = ROOT + str;
-        String s = Headers.IMAGE.getHeaderContent();
-        s = s.replace("<file_byte_size>", new File(str).length() + "");
+        String sheader = Headers.IMAGE.getHeaderContent();
+        sheader = sheader.replace("<file_byte_size>", new File(str).length() + "");
         String temp[] = str.split("\\.");
-        s = s.replace("<image_file_extension>", temp[temp.length - 1]);
+        sheader = sheader.replace("<image_file_extension>", temp[temp.length - 1]);
 
         try {
-            out = new BufferedOutputStream(clientSocket.getOutputStream());
-            out.write(s.getBytes());
+            bfout = new BufferedOutputStream(clientSocket.getOutputStream());
+            bfout.write(sheader.getBytes());
             BufferedInputStream bReader = new BufferedInputStream(new FileInputStream(str));
             byte[] b = new byte[1024];
             while ((bReader.read(b, 0, b.length)) != -1) {
-                out.write(b);
+                bfout.write(b);
             }
-            out.flush();
+            bfout.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (out != null) {
-                    out.close();
+                if (bfout != null) {
+                    bfout.close();
                 }
                 closeClientSocket();
             } catch (IOException e) {
